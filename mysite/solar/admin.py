ddd6cc5723fd655a297569862777c16ca3ad31d0
syuu-syuu <zhongle@unc.edu>
2024-08-19 11:14:08 +0800

    Register data models in admin

--- mysite/solar/admin.py
+++ mysite/solar/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Site, SiteMonthlyData, InverterData
+
+admin.site.register([Site, SiteMonthlyData, InverterData])
