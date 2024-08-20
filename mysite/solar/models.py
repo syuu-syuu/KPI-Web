9a0583f9999321b08f86c8bc6c6cc3f8a4f257f2
syuu-syuu <zhongle@unc.edu>
2024-08-20 23:05:38 +0800

    Update data models - add site city&state

--- mysite/solar/models.py
+++ mysite/solar/models.py
@@ -9,6 +9,61 @@ class Site(models.Model):
     latitude = models.DecimalField(max_digits=10, decimal_places=7)
     address = models.CharField(max_length=255)
 
+    STATE_CHOICES = [
+        ("AL", "Alabama"),
+        ("AK", "Alaska"),
+        ("AZ", "Arizona"),
+        ("AR", "Arkansas"),
+        ("CA", "California"),
+        ("CO", "Colorado"),
+        ("CT", "Connecticut"),
+        ("DE", "Delaware"),
+        ("FL", "Florida"),
+        ("GA", "Georgia"),
+        ("HI", "Hawaii"),
+        ("ID", "Idaho"),
+        ("IL", "Illinois"),
+        ("IN", "Indiana"),
+        ("IA", "Iowa"),
+        ("KS", "Kansas"),
+        ("KY", "Kentucky"),
+        ("LA", "Louisiana"),
+        ("ME", "Maine"),
+        ("MD", "Maryland"),
+        ("MA", "Massachusetts"),
+        ("MI", "Michigan"),
+        ("MN", "Minnesota"),
+        ("MS", "Mississippi"),
+        ("MO", "Missouri"),
+        ("MT", "Montana"),
+        ("NE", "Nebraska"),
+        ("NV", "Nevada"),
+        ("NH", "New Hampshire"),
+        ("NJ", "New Jersey"),
+        ("NM", "New Mexico"),
+        ("NY", "New York"),
+        ("NC", "North Carolina"),
+        ("ND", "North Dakota"),
+        ("OH", "Ohio"),
+        ("OK", "Oklahoma"),
+        ("OR", "Oregon"),
+        ("PA", "Pennsylvania"),
+        ("RI", "Rhode Island"),
+        ("SC", "South Carolina"),
+        ("SD", "South Dakota"),
+        ("TN", "Tennessee"),
+        ("TX", "Texas"),
+        ("UT", "Utah"),
+        ("VT", "Vermont"),
+        ("VA", "Virginia"),
+        ("WA", "Washington"),
+        ("WV", "West Virginia"),
+        ("WI", "Wisconsin"),
+        ("WY", "Wyoming"),
+    ]
+    city = models.CharField(max_length=100, default="Unknown")
+    state = models.CharField(max_length=2, choices=STATE_CHOICES, default="NC")
+
     MONTH_CHOICES = [(i, i) for i in range(1, 13)]
     contract_start_month = models.PositiveSmallIntegerField(choices=MONTH_CHOICES)
     contract_end_month = models.PositiveSmallIntegerField(choices=MONTH_CHOICES)
