16abd95b0244ec2c7a0634a87eba9b75656181c8
syuu-syuu <zhongle@unc.edu>
2024-09-15 02:21:11 +0800

    Handle meter power missing cases

--- mysite/solar/services/file_processor/check_missing.py
+++ mysite/solar/services/file_processor/check_missing.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 
+# Safe modification to the raw data
 def check_missing_irradiance(df):
     condition_missing = df["POA Irradiance"].isna()
     # Fill all the missings with -999
@@ -16,6 +17,25 @@ def check_missing_irradiance(df):
     return df
 
 
+def check_and_autofill_Meter(df):
+    condition_missing = df["Meter Power"].isna()
+    # condition_day = df["Day/Night"] == "Day"
+    # condition_day_missing = condition_missing & condition_day
+    # inverter_cols = [col for col in df.columns if col.startswith("Inverter_")]
+    # condition_at_least_one_inverter = df[inverter_cols].notna().any(axis=1)
+    # condition_can_be_filled = condition_missing & condition_at_least_one_inverter
+
+    df.loc[condition_missing, "Meter Power"] = -999
+    # if condition_day_missing.sum() > 0:
+    #     if condition_can_be_filled.sum() > 0:
+    #         df.loc[condition_can_be_filled, "Meter Power"] = df.loc[
+    #             condition_can_be_filled, inverter_cols
+    #         ].sum(axis=1)
+
+    return df
+
+
+# Safe modification to the raw data
 def fill_inverter_with_0(df, condition_off):
     INV_OUTAGE_THRESHOLD = 0.1
     inverter_cols = [col for col in df.columns if col.startswith("Inverter_")]
@@ -37,6 +57,7 @@ def check_missing(df):
     df.replace(" - ", np.nan, inplace=True)
 
     df = check_missing_irradiance(df)
+    df = check_and_autofill_Meter(df)
 
     # Check and autofill inverter
     # Step 1
