25930850454e5aad0544c5cf29600814d31daa8b
syuu-syuu <zhongle@unc.edu>
2024-09-15 22:32:30 +0800

    Refactor the code and add data validation

--- nextjs-dashboard/components/site-details-form.tsx
+++ nextjs-dashboard/components/site-details-form.tsx
@@ -1,11 +1,19 @@
-import React, { useEffect } from "react";
-import { SiteDetail } from '@/lib/definitions';
+import React, { useEffect, useState } from "react";
+import { SiteDetail, STATE_CHOICES } from '@/lib/definitions';
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
  
 import { Button } from "@/components/ui/button"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 import {
   Form,
   FormControl,
@@ -16,6 +24,8 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { render } from "react-dom";
+import { ST } from "next/dist/shared/lib/utils";
 
 interface SiteDetailsFormProps {
   siteDetails: SiteDetail | undefined;
@@ -27,10 +37,10 @@ const formSchema = z.object({
   address: z.string().optional(),
   city: z.string().optional(),
   state: z.string().optional(),
-  latitude: z.union([z.string(), z.number()]).optional(),  
-  longitude: z.union([z.string(), z.number()]).optional(), 
-  contract_start_month: z.union([z.string(), z.number()]).optional(), 
-  contract_end_month: z.union([z.string(), z.number()]).optional(),  
+  latitude: z.preprocess((val) => parseFloat(val as string), z.number().min(-90).max(90)),
+  longitude: z.preprocess((val) => parseFloat(val as string), z.number().min(-180).max(180)),
+  contract_start_month: z.preprocess((val) => parseInt(val as string, 10), z.number().int().min(1).max(12)),
+  contract_end_month: z.preprocess((val) => parseInt(val as string, 10), z.number().int().min(1).max(12)),
 });
 
 type FormSchemaKeys = keyof z.infer<typeof formSchema>;
@@ -40,16 +50,15 @@ const formFields: {
   name: FormSchemaKeys; 
   label: string; 
   placeholder: string; 
-  wrapperClassName?: string;  
 }[] = [
   { name: 'site_name', label: 'Site Name', placeholder: 'Enter site name' },
   { name: 'address', label: 'Address', placeholder: 'Enter address' },
-  { name: 'city', label: 'City', placeholder: 'Enter city', wrapperClassName: 'flex-1' },
-  { name: 'state', label: 'State', placeholder: 'Enter state', wrapperClassName: 'flex-1' },
-  { name: 'latitude', label: 'Latitude', placeholder: 'Enter latitude', wrapperClassName: 'flex-1' },
-  { name: 'longitude', label: 'Longitude', placeholder: 'Enter longitude', wrapperClassName: 'flex-1' },
-  { name: 'contract_start_month', label: 'Contract Start Month', placeholder: 'Enter contract start month', wrapperClassName: 'flex-1' },
-  { name: 'contract_end_month', label: 'Contract End Month', placeholder: 'Enter contract end month', wrapperClassName: 'flex-1' },
+  { name: 'city', label: 'City', placeholder: 'Enter city' },
+  { name: 'state', label: 'State', placeholder: 'Select state' },
+  { name: 'latitude', label: 'Latitude', placeholder: 'Enter latitude' },
+  { name: 'longitude', label: 'Longitude', placeholder: 'Enter longitude' },
+  { name: 'contract_start_month', label: 'Contract Start Month', placeholder: 'Enter contract start month'},
+  { name: 'contract_end_month', label: 'Contract End Month', placeholder: 'Enter contract end month'},
 ];
 
 
@@ -61,11 +70,11 @@ export const SiteDetailsForm: React.FC<SiteDetailsFormProps> = ({ siteDetails })
       site_name: siteDetails?.site_name || "",
       address: siteDetails?.address || "",
       city: siteDetails?.city || "",
-      state: siteDetails?.state || "",
-      latitude: siteDetails?.latitude || "",
-      longitude: siteDetails?.longitude|| "",
-      contract_start_month: siteDetails?.contract_start_month || "",
-      contract_end_month: siteDetails?.contract_end_month || "",
+      state: siteDetails?.state,
+      latitude: siteDetails?.latitude,
+      longitude: siteDetails?.longitude,
+      contract_start_month: siteDetails?.contract_start_month,
+      contract_end_month: siteDetails?.contract_end_month,
     },
   });
 
@@ -77,11 +86,11 @@ export const SiteDetailsForm: React.FC<SiteDetailsFormProps> = ({ siteDetails })
         site_name: siteDetails?.site_name || "",
         address: siteDetails?.address || "",
         city: siteDetails?.city || "",
-        state: siteDetails?.state || "",
-        latitude: siteDetails?.latitude || "",
-        longitude: siteDetails?.longitude || "",
-        contract_start_month: siteDetails?.contract_start_month || "",
-        contract_end_month: siteDetails?.contract_end_month || "",
+        state: siteDetails?.state,
+        latitude: siteDetails?.latitude,
+        longitude: siteDetails?.longitude,
+        contract_start_month: siteDetails?.contract_start_month,
+        contract_end_month: siteDetails?.contract_end_month,
       });
     }
   }, [siteDetails, form]);
@@ -91,105 +100,68 @@ export const SiteDetailsForm: React.FC<SiteDetailsFormProps> = ({ siteDetails })
     console.log("Form submitted", values);
   };
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
-        {/* Rendering grouped form fields */}
-        {['site_name', 'address'].map((name) => (
-          <FormField
-            key={name}
-            control={form.control}
-            name={name as FormSchemaKeys}
-            render={({ field }) => {
-              const currentField = formFields.find(f => f.name === name);
-              return (
-                <FormItem className="flex items-center space-x-4">
-                  <FormLabel className="flex-grow whitespace-nowrap">{currentField?.label}</FormLabel>
-                  <FormControl>
-                    <Input placeholder={currentField?.placeholder} {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-        ))}
-
-        {/* Render city and state in one line */}
-        <div className="flex space-x-4">
-          {['city', 'state'].map((name) => (
-            <FormField
-              key={name}
-              control={form.control}
-              name={name as FormSchemaKeys}
-              render={({ field }) => {
-                const currentField = formFields.find(f => f.name === name);
-                return (
-                  <FormItem  className={`${currentField?.wrapperClassName ?? ''} flex items-center space-x-4`}>
-                    <FormLabel className="flex-grow whitespace-nowrap">{currentField?.label}</FormLabel>
-                    <FormControl>
-                      <Input placeholder={currentField?.placeholder} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-          ))}
-        </div>
-
-        {/* Render latitude and longitude in one line */}
-        <div className="flex space-x-4">
-          {['latitude', 'longitude'].map((name) => (
-            <FormField
-              key={name}
-              control={form.control}
-              name={name as FormSchemaKeys}
-              render={({ field }) => {
-                const currentField = formFields.find(f => f.name === name);
-                return (
-                  <FormItem  className={`${currentField?.wrapperClassName ?? ''} flex items-center space-x-4`}>
-                    <FormLabel className="flex-grow whitespace-nowrap">{currentField?.label}</FormLabel>
-                    <FormControl>
-                      <Input placeholder={currentField?.placeholder} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-          ))}
-        </div>
-
-        {/* Render contract start and end month in one line */}
-        <div className="flex space-x-4">
-          {['contract_start_month', 'contract_end_month'].map((name) => (
-            <FormField
-              key={name}
-              control={form.control}
-              name={name as FormSchemaKeys}
-              render={({ field }) => {
-                const currentField = formFields.find(f => f.name === name);
-                return (
-                  <FormItem className={`${currentField?.wrapperClassName ?? ''} flex items-center space-x-4`} >
-                    <FormLabel className="flex-grow whitespace-nowrap">{currentField?.label}</FormLabel>
-                    <FormControl>
-                      <Input placeholder={currentField?.placeholder} {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-          ))}
-        </div>
-        <div className="grid justify-items-end">
-          <Button type="submit" >Submit</Button>
-        </div>
-        
-      </form>
-    </Form>
+  const renderInputField = (name: keyof z.infer<typeof formSchema>, label: string, type: string) => (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input placeholder={`Enter ${label.toLowerCase()}`} type={type} {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
   );
+
+  const renderSelectField = (name: keyof z.infer<typeof formSchema>, label: string, options: string[]) => (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <Select onValueChange={field.onChange} value={field.value?.toString()}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder={`Select ${label.toLowerCase()}`} />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {options.map(option => (
+                <SelectItem key={option} value={option}>
+                  {option}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+
+
+  return (
+  <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+      {/* Render each field individually */}
+      {formFields.map(({ name, label }) => {
+        return name === "state"
+          ? renderSelectField(name as keyof z.infer<typeof formSchema>, label, STATE_CHOICES)
+          : renderInputField(name as keyof z.infer<typeof formSchema>, label, "text");
+      })}
+
+      {/* Submit Button */}
+      <div className="grid justify-items-end">
+        <Button type="submit">Submit</Button>
+      </div>
+    </form>
+  </Form>
+);
+
 };
 
 
