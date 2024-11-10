a30eb463ea2df6c95e9c208b6b777fef76b49959
syuu-syuu <zhongle@unc.edu>
2024-11-10 21:37:22 +0800

    Correct the inset sidebar styling error

--- nextjs-dashboard/components/ui/sidebar.tsx
+++ nextjs-dashboard/components/ui/sidebar.tsx
@@ -146,7 +146,7 @@ const SidebarProvider = React.forwardRef<
               } as React.CSSProperties
             }
             className={cn(
-              "group/sidebar-wrapper flex min-h-svh w-full bg-sidebar",
+              "group/sidebar-wrapper flex min-h-svh w-full data-[variant=inset]:bg-sidebar",
               className
             )}
             ref={ref}
